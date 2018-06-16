7552a4617f5a6999fa09da990fbe897496ad95ea
daplay <daplay@gmail.com>
2018-06-15 19:41:14 -0700

    feat(gtd): more improvements for GTD on taskwarrior

--- environment/config/bash/scripts/taskwarrior.sh
+++ environment/config/bash/scripts/taskwarrior.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-if [ ! -z ${TASKRC+x} ]; then 
-  export PS1='$(task +in +PENDING count) \u \$ '
-  export TASKRC="$DOTFILES/environment/config/task/taskrc"
-fi
+# ideas taken from: https://taskwarrior.org/news/news.20150627.html
+#
+export PS1='$(task +in +PENDING count) \u \$ '
+export TASKRC="$DOTFILES/environment/config/task/taskrc"
 
-
-# taskwarrior
 alias in='task add +in'
 
 tickle() {
@@ -15,5 +13,23 @@ tickle() {
     in +tickle wait:$deadline $@
 }
 
+webpage_title (){
+    wget -qO- "$*" | hxselect -s '\n' -c  'title' 2>/dev/null
+}
+
+read_and_review (){
+    link="$1"
+    title=$(webpage_title $link)
+    echo $title
+    descr="\"Read and review: $title\""
+    id=$(task add +next +rnr "$descr" | sed -n 's/Created task \(.*\)./\1/p')
+    task "$id" annotate "$link"
+}
+
 alias tick=tickle
 alias think='tickle +1d'
+
+# alias to add things to research
+alias rnd='task add +rnd'
+# read and review
+alias rnr=read_and_review

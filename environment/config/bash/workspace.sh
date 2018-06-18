0f1549bd2b10df870c00dfb3ac8622c0955b96e4
Daniel Meneses <dmeneses@fitbit.com>
2018-06-18 09:29:02 -0700

    feat(workspace): now creates the directory if it doesnt exists

--- environment/config/bash/workspace.sh
+++ environment/config/bash/workspace.sh
@@ -6,11 +6,16 @@
 # Without arguments `workspace` executes pushd for $HOME/workspace
 # if an argument is passed it will execute pushd for $HOME/workspace/$argument
 #
+
 function workspace() {
+  local dirname="$HOME/workspace/"
+
   if [[ $# -eq 1 ]] ; then
-    cd $HOME/workspace/$1
-  else
-    cd $HOME/workspace
+    dirname="$HOME/workspace/$1"
+
+    # if it doesn't exists, create directory
+    [ -d $dirname ] || mkdir $dirname
   fi
+  pushd $dirname
 }
 

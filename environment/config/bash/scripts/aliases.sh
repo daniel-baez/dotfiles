da84645b1e75b7631a0c070b72bd6f1f259337a0
Daniel Meneses <dmeneses@fitbit.com>
2018-06-22 18:04:55 -0700

    feat(alias home)

--- environment/config/bash/scripts/aliases.sh
+++ environment/config/bash/scripts/aliases.sh
@@ -5,6 +5,7 @@ alias dct="dcb | cut -d_ -f1"
 alias dcr="git rev-parse --short=7 HEAD"
 
 alias dotfiles='pushd $DOTFILES'
+alias home='pushd $HOME'
 alias downloads='pushd $DOWNLOADS'
 alias tools='pushd $TOOLS'
 alias vimwiki='pushd $VIMWIKI'

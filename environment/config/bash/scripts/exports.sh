b1b4671ae670af52b3228164776fcc95bd1ae941
Daniel Meneses <dmeneses@fitbit.com>
2018-06-22 17:56:26 -0700

    feat(exports.sh): adds `JAVA_TOOL_OPTIONS='-Dfile.encoding=UTF8 -Duser.country=US -Duser.language=en'` to default exports

--- environment/config/bash/scripts/exports.sh
+++ environment/config/bash/scripts/exports.sh
@@ -5,5 +5,4 @@ export DOWNLOADS=$HOME/downloads
 export TOOLS=$HOME/tools
 export VIMWIKI=$HOME/vimwiki
 export WORKSPACE=$HOME/workspace
-
-
+export JAVA_TOOL_OPTIONS='-Dfile.encoding=UTF8 -Duser.country=US -Duser.language=en'
